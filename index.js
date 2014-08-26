37fc65e56e67fc5c983084d35ffe2b98c87be174
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-08-26 22:14:29 +0200

    set basic auth with predefined name

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
+var auth =require('http-auth');
 
-config = null;
-config_path = null;
+var config = null;
+var config_path = null;
 
-var setPassword = function() {
 
+var basic = auth.basic({
+  realm: "Cozy Light", function (username, password, callback) {
+    callback(username === "Tina" && password === "Bullock");
+  }
+});
+
+
+var setPassword = function(password) {
+  console.log("Password set is Bullock");
 }
 
+
 module.exports.configureAppServer = function(app, config, routes, callback) {
+  app.use(auth.connect(basic));
   callback();
 };
+
 
 module.exports.configure = function(options, config, program) {
   config = config;
   config_path = options.config_path;
 
   program
-    .command('set-password')
-    .description('Set web password to access to current Cozy Light instance')
+    .command('set-password <password>')
+    .description(
+        'Set web password to access to the current Cozy Light instance')
     .action(setPassword);
 };
